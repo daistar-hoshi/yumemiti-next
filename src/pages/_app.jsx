846bfc5fd683a719cd8ed74cfd980016c1813a3b
Hoshi <masa317dry@gmail.com>
2022-03-20 15:03:08 +0900

    ✨ Chakra-UI/Recoilの設定

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,20 @@
 import Head from 'next/head'
 import 'src/styles/globals.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { RecoilRoot } from 'recoil'
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
       <title>Yumemiti-クリエイター向け適職診断-</title>
     </Head>
-    <Component {...pageProps} />
+    <ChakraProvider>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ChakraProvider>
     </>
   )
 }
