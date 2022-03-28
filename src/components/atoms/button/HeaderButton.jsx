c39265303c542e96d93e106981eedd73ce2d0b95
Hoshi <masa317dry@gmail.com>
2022-03-28 22:15:59 +0900

    🩹  next/Linkの追加

--- src/components/atoms/button/HeaderButton.jsx
+++ src/components/atoms/button/HeaderButton.jsx
@@ -1,26 +1,29 @@
 import { Flex, Heading, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 import yumemitiIcon from "public/images/icon/yumemiti-icon.png";
 
 export default function HeaderButton() {
     return (
-        <Flex
-        align="center"
-        as="a"
-        _hover={{ cursor: "pointer", opacity: "0.8" }}
-        >
-            <Heading
-            as="h1" 
-            fontSize="30px"
+        <Link href="/">
+            <Flex
+            align="center"
+            as="a"
+            _hover={{ cursor: "pointer", opacity: "0.8" }}
             >
-                Yumemiti
-            </Heading>
-            <Image
-            src={yumemitiIcon.src} 
-            alt="menu"
-            boxSize="30px"
-            ml={1}
-            />
-        </Flex>
+                <Heading
+                as="h1" 
+                fontSize="30px"
+                >
+                    Yumemiti
+                </Heading>
+                <Image
+                src={yumemitiIcon.src} 
+                alt="menu"
+                boxSize="30px"
+                ml={1}
+                />
+            </Flex>
+        </Link>
     )
 }
